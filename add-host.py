b77ba662b768ddc9db0e91fe2764707e76a7ce25
Brian Farlinger <brian@lilac.red>
2016-08-08 16:54:39 -0700

    Update and rename new-host to add-host.py

--- add-host.py
+++ add-host.py
@@ -1,19 +1,20 @@
 #!/bin/python
 
+#place this file on the ansible server. I put it in /home/ansible/scripts/
 from pick import pick
 
-title = 'Please pick the best category which defines this server'
-options = ["web", "db", "kvm"]
-option, index = pick(options, title)
-filepath = "/home/brian/ansible/test"
-name = "test"
+#defining variables
+filepath = "/etc/ansible/hosts"
 
+#the hostname and option variable will come from the server which remotely runs this script using paramiko. 
+
+#writing to the file
 with open(filepath, "r") as in_file:
         buf = in_file.readlines()
 
 with open(filepath, "w") as out_file:
         for line in buf:
                 if line == "[" + option + "]\n":
-                        line = line + name + "\n"
+                        line = line + hostname + "\n"
                         print "Success! A line has been written to " + filepath
                 out_file.write(line)
